0b2399fbc785c4337bd3e85b380c9792d914e1c8
Leonardo Val <leonardo.val@creatartis.com>
2015-07-20 00:06:09 -0300

    Default modifiers
    + Sermat instances can now have default modifiers.
    + Also fixed registration of base types in new instances.

--- src/wrapup.js
+++ src/wrapup.js
@@ -2,9 +2,29 @@
 
 Here both `Sermat`'s prototype and singleton are set up. 
 */
-function sermat(obj, modifiers) {
-	return this.mat(this.ser(obj, modifiers));
+function Sermat(params) {
+	var __registry__ = {},
+		__register__ = register.bind(this, __registry__);
+	member(this, 'registry', __registry__);
+	member(this, 'register', __register__);
+	params = params || {};
+	this.mode = coalesce(params.mode, BASIC_MODE);
+	this.allowUndefined = coalesce(params.allowUndefined, false);
+	this.useConstructions = coalesce(params.useConstructions, true);
+	/** The constructors for Javascript's _basic types_ (`Boolean`, `Number`, `String`, `Object`, 
+		and `Array`, but not `Function`) are always registered. 
+	*/
+	__register__(['Boolean', 'Number', 'String', 'Object', 'Array']);
 }
+
+/** Sermat can be used as a constructor of serializer/materializer components as well as a 
+	singleton. Each instance has a separate registry of constructors.
+*/
+var __SINGLETON__ = new Sermat();
+
+/** The constructions for `Date` and `RegExp` are registered globally. 
+*/
+__SINGLETON__.register(['Date', 'RegExp']);
 
 (function (members) {
 	Object.keys(members).forEach(function (id) {
@@ -13,42 +33,34 @@ function sermat(obj, modifiers) {
 		member(Sermat, id, typeof m === 'function' ? m.bind(__SINGLETON__) : m);
 	});
 })({
-	'ALLOW_UNDEFINED': ALLOW_UNDEFINED,
-	'ALLOW_REPEATED': ALLOW_REPEATED,
-	'ALLOW_BINDINGS': ALLOW_BINDINGS,
-	'ALLOW_CIRCULAR': ALLOW_CIRCULAR,
-	'FORBID_CONSTRUCTIONS': FORBID_CONSTRUCTIONS,
+	'BASIC_MODE': BASIC_MODE,
+	'REPEAT_MODE': REPEAT_MODE,
+	'BINDING_MODE': BINDING_MODE,
+	'CIRCULAR_MODE': CIRCULAR_MODE,
 	'CONSTRUCTIONS': CONSTRUCTIONS,
 	
 	'identifier': identifier,
-	'construct': construct,
-	'materializeWithConstructor': materializeWithConstructor,
-	'serializeWithProperties': serializeWithProperties,
-	'type': type,
-	'signature': signature,
-	'checkSignature': checkSignature,
+	'record': record,
 	
-	'serialize': serialize,
-	'ser': serialize,
-	'materialize': materialize,
-	'mat': materialize,
-	'sermat': sermat
+	'serialize': serialize, 'ser': serialize,
+	'serializeWithProperties': serializeWithProperties,
+	
+	'materialize': materialize, 'mat': materialize,
+	'construct': construct,
+	'type': type,
+	'signature': signature, 'checkSignature': checkSignature,
+	'materializeWithConstructor': materializeWithConstructor,
+	
+	'sermat': function sermat(obj, modifiers) {
+		return this.mat(this.ser(obj, modifiers));
+	}
 });
-member(Sermat, 'register', __SINGLETON__['register']);
-member(Sermat, 'record', __SINGLETON__['record']);
-
-/** The constructors that are registered globally `Sermat` are: `Boolean`, `Number`, `String`, 
-`Object`, `Array`, `Date` and `RegExp`. 
-*/
-[Boolean, Number, String, Object, Array, Date, RegExp
-].forEach(function (type) {
-	var rec = record(CONSTRUCTIONS, type);
-	Sermat.register(type, rec.serializer, rec.materializer);
-});
+member(Sermat, 'registry', __SINGLETON__.registry);
+member(Sermat, 'register', __SINGLETON__.register);
 
 /** Module layout (not frozen in purpose).
 */
-Sermat.__package__ = 'sermat';
-Sermat.__name__ = 'Sermat';
+member(Sermat, '__package__', 'sermat');
+member(Sermat, '__name__', 'Sermat');
 Sermat.__init__ = __init__;
 Sermat.__dependencies__ = [];
