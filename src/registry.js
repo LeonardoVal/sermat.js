e9dfcb762e7ed4b755b11c75b9ce2356e1f39682
Leonardo Val <leonardo.val@creatartis.com>
2015-07-27 00:12:37 -0300

    Registry rework.
    + Added `include()` to register with flexibility.
    + Changed `register()` to take an object.
    + Allowed to specify `global` constructions.

--- src/registry.js
+++ src/registry.js
@@ -8,109 +8,132 @@ RegExp("\d+", "g")
 Date(1999,12,31,23,59,59,999)
 ```
 
-These are called _constructions_. In order to use this, the custom class' constructor has to be 
-registered with a serializer (unparser or _stringifier_) and a materializer (parser or deserializer) 
-functions.
-*/
-function entry(identifier, constructor, serializer, materializer) {
-	var r = { 
-		identifier: identifier,
-		constructor: constructor, 
-		serializer: serializer, 
-		materializer: materializer 
-	};
-	Object.freeze(r);
-	return r;
-}
+These are called _constructions_. In order to use them, the custom class' constructor has to be 
+registered with two functions: serializer and materializer. The serializer calculates an array of
+values that will allow to rebuild (i.e. materialize) the instance being serialized (i.e. 
+_stringified_). The materializer creates a new instance based on the previously serialized values.
 
-/** All constructions use a name to identify the type's custom serializer and materializer. Sermat 
-must be able to infer this name from the constructor function of the type. By default the name of 
-the constructor function is used, but this can be overriden by setting a `__SERMAT__` property
-of the function.
+All constructions use a name to identify the type's custom serializer and materializer. Sermat must 
+be able to infer this name from the constructor function of the type. By default the name of the 
+constructor function is used, but this can be overriden by setting a `__SERMAT__` property of the 
+function.
 */
 var FUNCTION_ID_RE = /^\s*function\s+([\w\$]+)/,
 	ID_REGEXP = /^[\$A-Z_a-z][\$\-\.\w]*$/;
-function identifier(constructor, must) {
-	var id = (constructor.__SERMAT__ && constructor.__SERMAT__.identifier)
-		|| constructor.name
+function identifier(type, must) {
+	var id = (type.__SERMAT__ && type.__SERMAT__.identifier)
+		|| type.name
 		|| (FUNCTION_ID_RE.exec(constructor +'') || [])[1];
 	if (!id && must) {
-		raise('identifier', "Could not found id for constructor!", { constructorWithoutId: constructor });
+		raise('identifier', "Could not found id for type!", { type: type });
 	}
 	return id;
 }
 
-/** A `record` for a construction can be obtained using its identifier or its constructor function. 
-If a function is given that is not registered, it will be registered if possible.
+/** A `record` for a construction can be obtained using its identifier or the constructor function
+of the type.
 */
-function record(constructor) {
-	if (typeof constructor === 'string') {
-		return this.registry[constructor];
-	} else {
-		var id = identifier(constructor, true),
+function record(type, must) {
+	var id = typeof type === 'function' ? identifier(type, true) : type +'',
 		result = this.registry[id];
-		return result || this.register(constructor);
+	if (!result && must) {
+		raise('record', 'Unknown type "'+ id +'"!', { type: type });
 	}
+	return result;
 }
 
-/** The registry for every custom serializer has three components: an identifier, a serializer 
-(unparser or _stringifier_) function and a materializer (parser or deserializer) function. All of 
-these can be taken from a member of the constructor function called `__SERMAT__`. Else, both the 
-constructor's name is used as identifier and at least the serializer has to be given.
+/** The registry spec for every custom construction usually has four components: an `identifier`, a 
+`type` constructor function, a `serializer` function and a `materializer` function. A `global` flag
+can also be provided, and if true causes the construction to be added to the `Sermat.CONSTRUCTIONS` 
+global registry.
 
-If a materializer function is not specified, it is assumed the serialization is equal to the
-arguments with which the constructor has to be called to recreate the instance. So, a default
-materializer is created, which calls the constructor with the list of values in the text.
+The identifier can be inferred from the constructor function. If a materializer function is not 
+specified, it is assumed the serialization is equal to the arguments with which the constructor has 
+to be called to recreate the instance. So, a default materializer is created, which calls the 
+constructor with the list of values in the text.
 */
-function register(registry, constructor, serializer, materializer) {
-	switch (typeof constructor) {
-		case 'function': break;
-		case 'string': return register(registry, CONSTRUCTIONS[constructor]);
-		case 'object': {
-			if (Array.isArray(constructor)) {
-				return constructor.map(function (c) {
-					return register(registry, c);
-				});
-			} else {
-				return register(registry, constructor.constructor, constructor.serializer, constructor.materializer);
-			}
-		}
-		default: raise('register', "Constructor is not a function!", { invalidConstructor: constructor });
+function register(registry, spec) {
+	if (typeof spec.type !== 'function') {
+		raise('register', 'No constructor found for type ('+ spec +')!', { spec: spec });
 	}
-	
-	var id = identifier(constructor, true);
+	var id = spec.identifier;
+	if (!id) {
+		id = spec.identifier = identifier(spec.type, true);
+	}
 	if (!ID_REGEXP.exec(id)) {
-		raise('register', "Invalid identifier '"+ id +"'!", { invalidId: id });
+		raise('register', "Invalid identifier '"+ id +"'!", { spec: spec });
 	}
 	if (registry.hasOwnProperty(id)) {
-		raise('register', "'"+ id +"' is already registered!", { repeatedId: id });
+		raise('register', "'"+ id +"' is already registered!", { spec: spec });
 	}
-	var custom = constructor.__SERMAT__;
-	if (typeof serializer === 'undefined') {
-		serializer = custom && custom.serializer
+	if (typeof spec.serializer !== 'function') {
+		raise('register', "Serializer for '"+ spec.identifier +"' is not a function!", { spec: spec });
 	}
-	if (typeof serializer !== 'function') {
-		raise('register', "Serializer for '"+ id +"' is not a function!", { invalidSerializer: serializer });
+	if (!spec.materializer) {
+		spec.materializer = materializeWithConstructor.bind(this, spec.type);
 	}
-	if (typeof materializer === 'undefined') {
-		materializer = custom && custom.materializer;
-		if (typeof materializer !== 'function') {
-			materializer = materializeWithConstructor.bind(this, constructor);
-		}
+	if (typeof spec.materializer !== 'function') {
+		raise('register', "Materializer for '"+ id +"' is not a function!", { spec: spec });
 	}
-	if (typeof materializer !== 'function') {
-		raise('register', "Materializer for '"+ id +"' is not a function!", { invalidMaterializer: materializer });
+	Object.freeze(spec);
+	registry[id] = spec;
+	if (spec.global && !CONSTRUCTIONS[id]) {
+		CONSTRUCTIONS[id] = spec;
 	}
-	return registry[id] = entry(id, constructor, serializer, materializer);
+	return spec;
 }
 
-/** `materializeWithConstructor` is a generic way of creating a new instance of the given 
-`constructor`. Basically a new object is built using the constructor's prototype, and then the
-constructor is called on this object and the given arguments (`args`) to initialize it.
+/** The `include` method is a more convenient and flexible way of registering custom types. If a 
+name (i.e. a string) is provided, the corresponding entry in `Sermat.CONSTRUCTIONS` will be added.
+If a constructor function is given and it has a `__SERMAT__` member with the type's definitions, 
+then this will be registered. An array with a combination of the previous two types registers all
+members. Lastly, an spec record can be used as well. The method tries not to raise errors. 
+*/
+function include(arg) {
+	var spec = null;
+	switch (typeof arg) {
+		case 'function': {
+			spec = arg.__SERMAT__;
+			if (spec && !this.record(arg)) {
+				spec.type = arg;
+				spec = this.register(spec);
+			}
+			break;
+		}
+		case 'string': {
+			if (CONSTRUCTIONS[arg] && !this.registry[arg]) {
+				spec = this.register(CONSTRUCTIONS[arg]);
+			}
+			break;
+		}
+		case 'object': {
+			if (Array.isArray(arg)) {
+				return arg.map((function (c) {
+					return this.include(c);
+				}).bind(this));
+			} else {
+				spec = arg;
+				if (spec.identifier ? !this.registry[spec.identifier] : !this.record(spec.constructor)) {
+					spec = this.register(spec);
+				}
+			}
+			break;
+		}
+		default: raise('register', "Could not include ("+ arg +")!", { arg: arg });
+	}
+	if (spec && spec.include) {
+		this.include(spec.include);
+	}
+	return spec;
+}
+
+/** `materializeWithConstructor` is a generic way of creating a new instance of the given type
+`constructor`. Basically a new object is built using the type's prototype, and then the constructor 
+is called on this object and the given arguments (`args`) to initialize it.
 
 This method can be used to quickly implement a materializer function when only a call to a 
 constructor function is required. It is the default materialization when no method has been given 
-for a registered constructor.
+for a registered type.
 */
 function materializeWithConstructor(constructor, obj, args) {
 	if (!obj) {
