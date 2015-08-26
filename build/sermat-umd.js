55b9919d3ba325f2f239ed5729b119fe695ae5e2
Leonardo Val <leonardo.val@creatartis.com>
2015-08-26 13:16:37 -0300

    New build.

--- build/sermat-umd.js
+++ build/sermat-umd.js
@@ -634,16 +634,15 @@ function materialize(text) {
 with some of its properties. This method can be used to quickly implement a serializer function when 
 the constructor of the type can be called with an object.
 */
-function serializeAsProperties(obj, properties) {
-	var result = {};
-	if (Array.isArray(properties)) {
-		properties.forEach(function (k) {
-			result[k] = obj[k];
-		});
-	} else {
-		Object.keys(properties).forEach(function (k) {
-			result[k] = obj[properties[k]];
-		});
+function serializeAsProperties(obj, properties, ownProperties) {
+	var result = {}, 
+		fromArray = Array.isArray(properties),
+		name;
+	for (var i in properties) {
+		name = properties[i];
+		if (!ownProperties || obj.hasOwnProperty(name)) {
+			result[fromArray ? name : i] = obj[name];
+		}
 	}
 	return [result];
 }
@@ -671,6 +670,94 @@ function materializeWithConstructor(constructor, obj, args) {
 */
 function sermat(obj, modifiers) {
 	return this.mat(this.ser(obj, modifiers));
+}
+
+/** ## Special encoding of byte arrays with base 85 ################################################
+
+This file holds a custom base 85 encoding (similar to [ascii85](https://en.wikipedia.org/wiki/Ascii85)) 
+of Javascript's byte arrays. It is more space efficient than base64. Assuming UTF8 text enconding, 
+each 100 characters in base 64 encoded strings hold around 75 bytes, while 100 characters in base 85
+hold around 80 bytes.
+
+The characters used are in the range `[\x21-\x7F]` excluing `"$%&'``<>\`. These are special 
+characters in XML and in the syntax of string literals in many programming language and macro 
+systems. Not using these characters allows the encoded strings to be embedded in XML and string 
+literals safely without requiring escape sequences.
+*/
+var CHARS85 = '!#()*+,-./0123456789:;=?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_abcdefghijklmnopqrstuvwxyz{|}~',
+	DIGITS85 = (function () {
+		var r = {};
+		for (var i = 0; i < 85; i++) {
+			r[CHARS85.charAt(i)] = i;
+		}
+		return r;
+	})();
+
+function enc85(num) {
+	var result = '', div;
+	while (num !== 0 || result.length < 5) {
+		div = Math.floor(num / 85);
+		result = CHARS85[num - div * 85] + result;
+		num = div;
+	}
+	return result;
+}
+	
+function encode85(buffer) {
+	var view = new DataView(buffer),
+		result = '', i = 0, len = view.byteLength;
+	switch (len % 4) {
+		case 1: result += enc85(0x101010000 + view.getUint8(i++)); break;
+		case 2: result += enc85(0x101000000 + view.getUint8(i++) * 0x100 + 
+			view.getUint8(i++)); break;
+		case 3: result += enc85(0x100000000 + view.getUint8(i++) * 0x10000 +
+			view.getUint8(i++) * 0x100 + view.getUint8(i++)); break;
+	}
+	while (i < len) {
+		result += enc85(view.getUint8(i++) * 0x1000000 + view.getUint8(i++) * 0x10000 +
+			view.getUint8(i++) * 0x100 + view.getUint8(i++));
+	}
+	return result;
+}
+
+function dec85(str) {
+	var result = 0;
+	for (var i = 0, len = str.length; i < len; i++) {
+		result = DIGITS85[str[i]] + 85 * result;
+	}
+	return result;
+}
+
+function decode85(string) {
+	var len = string.length, i = 0, j = 0,
+		buffer, view, num;
+	if (len < 1) {
+		return new ArrayBuffer(0);
+	}
+	num = dec85(string.substr(0, 5));
+	buffer = new ArrayBuffer((len / 5 - 1) * 4 + 
+		(num < 0x100000000 ? 4 : num < 0x101000000 ? 3 : num < 0x101010000 ? 2 : 1)
+	);
+	view = new DataView(buffer);
+	len = buffer.byteLength;
+	if (num < 0x100000000) {
+		view.setUint8(i++, Math.floor(num / 0x1000000));
+	}
+	if (num < 0x101000000) {
+		view.setUint8(i++, (num & 0xFF0000) >> 16);
+	}
+	if (num < 0x101010000) {
+		view.setUint8(i++, (num & 0xFF00) >> 8);
+	}
+	view.setUint8(i++, num & 0xFF);
+	while (i < len) {
+		num = dec85(string.substr(j += 5, 5));
+		view.setUint8(i++, Math.floor(num / 0x1000000)); // Cannot use bitwise because 32 bits are signed.
+		view.setUint8(i++, (num & 0xFF0000) >> 16);
+		view.setUint8(i++, (num & 0xFF00) >> 8);
+		view.setUint8(i++, num & 0xFF);
+	}
+	return buffer;
 }
 
 /** ## Constructions for Javascript types ##########################################################
@@ -879,27 +966,29 @@ function Sermat(params) {
 }
 
 var __members__ = {
-	'BASIC_MODE': BASIC_MODE,
-	'REPEAT_MODE': REPEAT_MODE,
-	'BINDING_MODE': BINDING_MODE,
-	'CIRCULAR_MODE': CIRCULAR_MODE,
-	'CONSTRUCTIONS': CONSTRUCTIONS,
+	BASIC_MODE: BASIC_MODE,
+	REPEAT_MODE: REPEAT_MODE,
+	BINDING_MODE: BINDING_MODE,
+	CIRCULAR_MODE: CIRCULAR_MODE,
+	CONSTRUCTIONS: CONSTRUCTIONS,
 	
-	'identifier': identifier,
-	'record': record,
-	'include': include,
-	'exclude': exclude,
+	identifier: identifier,
+	record: record,
+	include: include,
+	exclude: exclude,
 	
-	'serialize': serialize, 'ser': serialize,
-	'serializeAsProperties': serializeAsProperties,
+	serialize: serialize, ser: serialize,
+	serializeAsProperties: serializeAsProperties,
+	serializeAsType: serializeAsType,
+	signature: signature, checkSignature: checkSignature,
 	
-	'materialize': materialize, 'mat': materialize,
-	'construct': construct,
-	'signature': signature, 'checkSignature': checkSignature,
-	'serializeAsType': serializeAsType,
-	'materializeWithConstructor': materializeWithConstructor,
+	materialize: materialize, mat: materialize,
+	construct: construct,
+	materializeWithConstructor: materializeWithConstructor,
 	
-	'sermat': sermat
+	encode85: encode85, decode85: decode85,
+	
+	sermat: sermat
 };
 Object.keys(__members__).forEach(function (id) {
 	var m = __members__[id];
